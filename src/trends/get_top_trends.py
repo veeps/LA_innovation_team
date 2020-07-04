21a5351e6fb020e27f14cb0a4b00fd26d0bc2121
veeps <vpeng12@gmail.com>
2020-07-04 16:28:37 -0700

    making code modules

---
+++ src/trends/get_top_trends.py
@@ -0,0 +1,17 @@
+"""
+Functions to explore trends in the last 5 years
+"""
+
+import pandas as pd
+
+
+def top_trends(df, column, variable, n):
+	"""
+	Gets the top trends for the variable you're interested in.
+
+	column = column to groupby (string)
+	variable = column name of the variable you're interested to explore (string)
+	n = how many of the top values do you want to see? (integer)
+	"""
+	top = df.groupby(column)[variable].value_counts(ascending = False)
+	return top.groupby(column).nlargest(n).reset_index(level=1, drop = True)
